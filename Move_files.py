9b33d02c17ce74de1fabe6b4d7c17b6efc05849d
Gilles <57911632+Gilles-D@users.noreply.github.com>
2020-06-14 15:45:42 +0200

    Add files via upload

---
+++ Move_files.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 11:47:33 2020
+
+@author: MOCAP
+"""
+
+
+import cv2
+import numpy as np
+import glob
+import os
+import re
+import shutil
+
+rootdir = 'C:/Users/MOCAP/Pictures/IC Express/'
+
+img_array = []
+
+
+for subdirs, dirs, files in os.walk(rootdir):
+    start_index = []
+    folder = 1
+    for file in files:
+        result = file.endswith('_1_0.bmp')
+        if result == True:
+            start_index.append(files.index(file))
+    
+    while folder <= len(start_index):
+        if folder == 1:
+            if not os.path.exists("{}/{}".format(subdirs, folder)):
+                os.makedirs("{}/{}".format(subdirs, folder))
+            print(folder)    
+            for file in files[0:start_index[folder]]:
+               shutil.move("{}/{}".format(subdirs,file), "{}/{}/{}".format(subdirs, folder,file))
+            folder+=1
+        
+        else:
+
+              
+            if not folder == len(start_index):
+                print(folder)
+                if not os.path.exists("{}/{}".format(subdirs, folder)):
+                    os.makedirs("{}/{}".format(subdirs, folder))
+                for file in files[start_index[folder-1]:start_index[folder]]:
+                    shutil.move("{}/{}".format(subdirs,file), "{}/{}/{}".format(subdirs, folder,file))
+                folder+=1
+                
+            if folder == len(start_index):
+                print(folder)
+                if not os.path.exists("{}/{}".format(subdirs, folder)):
+                    os.makedirs("{}/{}".format(subdirs, folder))
+                for file in files[start_index[folder-1]:]:
+                    shutil.move("{}/{}".format(subdirs,file), "{}/{}/{}".format(subdirs, folder,file))
+                folder+=1
