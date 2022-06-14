2fa7164f0c2dc2e6f9e4e50818a66fb74069c7f4
Gilles <gillesdelbecq@hotmail.fr>
2022-06-14 19:43:26 +0200

    Convert

---
+++ In vivo Multi/Convert_H5_to_RBF.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 11:57:30 2019
+@author: lspaeth (modified by flareno)
+Created on Mon Nov 12 14:14:18 2018
+This class loads HdF5 recordings from MCS acquisition system as matrices of shape ((channel,data))
+Allows to load Raw signals
++ associated time vectors
++ associated sampling rates
+All in Volts and Seconds 
+Hope it will work 
+Then all you have to do is to load HdF5IO from eletroPy package; init class with smthg = HdF5IO(filepath)
+After that u can load every instance with associated function, they are all described bellow. 
+"""
+import matplotlib.pyplot as plt
+import os
+
+class HdF5IO:
+    
+    def __init__(self,filepath):
+        import h5py as h5
+        file_ = h5.File(filepath,'r')
+        
+        self.file = file_['Data'] #Loads first node 
+
+#----------RAW RECORDINGS---------------------------------------------------------------------------------------------   
+    def raw_record(self): #Gets Raw Records as matrix ((channel,data))
+        
+        raw = self.file['Recording_0']['AnalogStream']['Stream_0']['ChannelData']
+        
+        import numpy as np 
+        raw_record = np.zeros((raw.shape[0],raw.shape[1]))
+        raw_conv = float(self.file['Recording_0']['AnalogStream']['Stream_0']['InfoChannel'][0][10]) #Scaling Factor 
+        
+        for i in range(raw.shape[0]): #Stores data in new matrix 
+            raw_record[i,:] = raw[i,:]/raw_conv #From pV to V
+    
+        return raw_record
+    
+    def raw_time(self): #Gets time vector for raw records 
+        import numpy as np
+        raw_tick = int(self.file['Recording_0']['AnalogStream']['Stream_0']['InfoChannel'][0][9])/1000000.0 #exp6 to pass from us to s
+        raw_length = len(self.file['Recording_0']['AnalogStream']['Stream_0']['ChannelData'][0])        
+        raw_time = np.arange(0,raw_length*raw_tick,raw_tick)        
+        return raw_time
+        
+    def raw_sampling_rate(self): #Gets sampling rate
+        
+        raw_tick = float(self.file['Recording_0']['AnalogStream']['Stream_0']['InfoChannel'][0][9])/1000000.0
+        
+        return 1.0/raw_tick #In Hz
+    
+    
+
+#---------CONVERT H5 to RAW BINARY-----------------------------------------------------------------------------------
+
+def convert_folder(folderpath, newpath, data_type='raw'):
+    
+    import os, re
+    import numpy as np
+    
+    list_dir = os.listdir(folderpath)
+#    folderpath = folderpath
+#    newpath = newpath
+    
+    for file in list_dir:
+        
+        if file.endswith('.h5'):
+
+            print ('Converting ' + file + '...')
+            new_path = '%s/%s'%(folderpath,file)
+        
+            data = HdF5IO(new_path)
+
+            traces = data.raw_record()
+            sampling_rate = int(data.raw_sampling_rate())
+            
+            name = re.sub('\.h5$', '', file)
+        
+            file_save = '%s/%s_%sHz.rbf'%(newpath,name,sampling_rate)
+        
+            with open(file_save, mode='wb') as file : 
+
+                    traces.tofile(file,sep='')                    
+                
+            print ('Conversion DONE')
+            
+        else:
+            print (file + ' is not an h5 file, will not be converted')
+            
+    print ('Whole directory has been converted successfully')
+                
+                
+
+if __name__ == '__main__':
+    
+    experience_list = []
+
+
+    folderpath = rf'D:\Working_Dir\In vivo Mars 2022\H5' #Folder containing H5 files, with a subdirectory for each experience (day)
+    
+    experience_list=['02-07', '02-14', '02-15', '03-22', '03-24', '03-29', '03-30', '04-08', '05-24', '05-25', '05-30', '06-01', '06-02', '06-10']
+    
+    # for subfolder in os.walk(folderpath):
+    #     experience_list.append((subfolder[0].split('\\'))[-1])  #List all experiences
+    
+    # if experience_list[0]=='H5':
+    #     experience_list.pop(0) #Pop H5 main folder from the list
+    
+    
+    for experience in experience_list:
+        H5_path=rf'D:\Working_Dir\In vivo Mars 2022\H5\{experience}'
+        RBF_path = rf'D:\Working_Dir\In vivo Mars 2022\RBF\{experience}'
+        
+        isExist = os.path.exists(RBF_path)
+        if not isExist:
+            os.makedirs(RBF_path) #Create folder for the experience if it is not already done
+            
+        convert_folder(H5_path, RBF_path) #Convert all the files from the experience folder
+    
+    
+
+    
+
+     
+    
+    
