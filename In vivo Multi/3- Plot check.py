0b5733e5b653e34b63278b7da48b5db2363dbd74
Gilles <gillesdelbecq@hotmail.fr>
2022-06-23 15:51:32 +0200

    plot check loop on files in an exp (date) directory

--- In vivo Multi/3- Plot check.py
+++ In vivo Multi/3- Plot check.py
@@ -16,76 +16,113 @@ Parameters
 '''
 sampling_rate = 20000
 
-path = r'D:/Working_Dir/In vivo Mars 2022/RBF/06-15/raw/2209_04_0006_20000Hz.rbf'
+# path = r'D:/Working_Dir/In vivo Mars 2022/RBF/06-15/raw/2209_04_0006_20000Hz.rbf'
 
-Save=False
+folderpath = r'D:\Working_Dir\In vivo Mars 2022\RBF\06-15/' #use / at the end
+Animal='2209'
+
+Save=True
+plot_format='png'
 Preprocessed=True
 Indivdual_plots=True
+Autoclose=True
+
+
+
+
+
+
+
+
+
+
 
 """
 Setups
 """
-raw_file = np.fromfile(path)
-name_file=path.split('/')[-1].split('.')[0]
-file = raw_file.reshape(int(len(raw_file)/16),-1).transpose()
+raw_path=rf'{folderpath}/raw/'
+files_to_analyse=[]
+
+for file_name in os.listdir(raw_path):
+    if file_name.endswith('.rbf') and Animal in file_name and not "filtered" in file_name and not "concatenated" in file_name and not "Merge" in file_name:
+        files_to_analyse.append(rf'{raw_path}/{file_name}')
+
+if Save ==True:
+    plot_save = rf"{folderpath}/plots"
+    isExist = os.path.exists(plot_save)
+    if not isExist:
+        os.makedirs(plot_save) #Create folder for the experience if it is not already done
 
 
 
-if Preprocessed == True:
-    preprocessed_path=rf"{path.split('raw')[0]}/preprocessed"
+for file_to_analyze in files_to_analyse:
     
-    path_filter = rf"{preprocessed_path}/{name_file}_filtered.rbf"
-    path_cmr = rf"{preprocessed_path}/{name_file}_cmr.rbf"
-    file_filtered=np.fromfile(path_filter).reshape(16,-1)
-    file_cmr=np.fromfile(path_cmr).reshape(16,-1)
-
-time_vector = np.arange(0,len(file[0])/sampling_rate,1/sampling_rate)
-
-
-
-"""
-Plot every channel (raw, filtered, cmr) on individual plot
-"""
-if Indivdual_plots==True:
+    raw_file = np.fromfile(file_to_analyze)
     
-    for i in range(len(file)):
-        plt.figure()
-        plt.title(rf'Channel {i}')
-        # plt.plot(time_vector,file[i,:],alpha=1,linewidth=1)
-        if Preprocessed == True:
-            plt.plot(time_vector,file_filtered[i,:],alpha=0.5)
-            plt.plot(time_vector,file_cmr[i,:])
+    name_file=file_to_analyze.split('/')[-1].split('.')[0]
     
+    print(name_file)
     
-"""
-Plot all channel cmr on 1 plot
-"""   
-if Preprocessed == True:
-    fig1, axs = plt.subplots(len(file_cmr),sharex=True,sharey=True)
-    fig1.suptitle(f'{path.split("/")[-1]} CMR of all channels')
-    for i in range(len(file_cmr)):
-        axs[i].plot(time_vector,file_cmr[i,:])
-        axs[i].get_yaxis().set_visible(False)
-
-
-"""
-Plot all channel raw on 1 plot
-"""
-fig2, axs = plt.subplots(len(file),sharex=True,sharey=True)
-fig2.suptitle(f'{path.split("/")[-1]} Raw signal of all channels')
-for i in range(len(file)):
-    axs[i].plot(time_vector,file[i,:])
-    axs[i].get_yaxis().set_visible(False)
-    
-"""
-Save plots
-"""
-plot_save = rf"{path.split('raw')[0]}/plots"
-isExist = os.path.exists(plot_save)
-if not isExist:
-    os.makedirs(plot_save) #Create folder for the experience if it is not already done
-    
-if Save==True:
+    data = raw_file.reshape(int(len(raw_file)/16),-1).transpose()
+        
     if Preprocessed == True:
-        fig1.savefig(rf'{plot_save}/{name_file}_cmr.png')
-    fig2.savefig(rf'{plot_save}/{name_file}_raw.png')
+        preprocessed_path=rf"{file_to_analyze.split('raw')[0]}/preprocessed"
+        path_filter = rf"{preprocessed_path}/{name_file}_filtered.rbf"
+        path_cmr = rf"{preprocessed_path}/{name_file}_cmr.rbf"
+        
+        data_filtered=np.fromfile(path_filter).reshape(16,-1)
+        data_cmr=np.fromfile(path_cmr).reshape(16,-1)
+    
+    time_vector = np.arange(0,len(data[0])/sampling_rate,1/sampling_rate)
+    
+       
+    
+    """
+    Plot every channel (raw, filtered, cmr) on individual plot
+    """
+    if Indivdual_plots==True:
+        
+        for i in range(len(data)):
+            plt.figure()
+            plt.title(rf'Channel {i}')
+            # plt.plot(time_vector,data[i,:],alpha=1,linewidth=1)
+            if Preprocessed == True:
+                plt.plot(time_vector,data_filtered[i,:],alpha=0.5)
+                plt.plot(time_vector,data_cmr[i,:])
+            
+            if Save == True:
+                plt.savefig(rf'{plot_save}/{name_file}_{i}.{plot_format}')
+                
+        if Autoclose == True:
+            plt.close('all')
+        
+    """
+    Plot all channel cmr on 1 plot
+    """   
+    if Preprocessed == True:
+        fig1, axs = plt.subplots(len(data_cmr),sharex=True,sharey=True)
+        fig1.suptitle(f'{file_to_analyze.split("/")[-1]} CMR of all channels')
+        for i in range(len(data_cmr)):
+            axs[i].plot(time_vector,data_cmr[i,:])
+            axs[i].get_yaxis().set_visible(False)
+        
+        if Autoclose == True:
+            plt.close()    
+    
+    """
+    Plot all channel raw on 1 plot
+    """
+    fig2, axs = plt.subplots(len(data),sharex=True,sharey=True)
+    fig2.suptitle(f'{file_to_analyze.split("/")[-1]} Raw signal of all channels')
+    for i in range(len(data)):
+        axs[i].plot(time_vector,data[i,:])
+        axs[i].get_yaxis().set_visible(False)
+    if Autoclose == True:
+        plt.close()        
+    """
+    Save plots
+    """
+    if Save==True:
+        if Preprocessed == True:
+            fig1.savefig(rf'{plot_save}/{name_file}_cmr.{plot_format}')
+        fig2.savefig(rf'{plot_save}/{name_file}_raw.{plot_format}')
