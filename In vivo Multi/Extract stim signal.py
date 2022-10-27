43e95162229629466ba2dfa98e4af52ef050e81e
Gilles <gillesdelbecq@hotmail.fr>
2022-10-27 10:31:05 +0200

    Update Extract stim signal.py

--- In vivo Multi/Extract stim signal.py
+++ In vivo Multi/Extract stim signal.py
@@ -8,137 +8,147 @@ Created on Fri Oct 21 15:28:47 2022
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import scipy.signal as sp
+
+
 
 '''
 Parameters
 '''
 sampling_rate = 20000
-sampling_rate_stim = 52.465
 
+sampling_rate_stim = 1000000
 
-signal_file = r'//equipe2-nas1/Gilles.DELBECQ/Data/ePhy/Cohorte 1/RBF/10-20/preprocessed/0004_06_01_0007_20000Hz_cmr.rbf' #use / at the end
+signal_file = r'//equipe2-nas1/Gilles.DELBECQ/Data/ePhy/Cohorte 1/RBF/10-24/preprocessed/0004_07_0005_20000Hz_cmr.rbf' #use / at the end
 number_of_channel=6
 
-stim_idx_file=r'//equipe2-nas1/Gilles.DELBECQ/Data/ePhy/Cohorte 1/stim_idx/10-20/0004_06_01_0007.txt'
+stim_idx_file=r'//equipe2-nas1/Gilles.DELBECQ/Data/ePhy/Cohorte 1/stim_idx/10-24/0004_07_0005.txt'
 
 Save=True
 
 
-signal=np.fromfile(signal_file)
-signal=signal.reshape(int(len(signal)/number_of_channel),-1).transpose()
 
-stim_indexes=np.loadtxt(stim_idx_file)
-stim_times=stim_indexes/sampling_rate_stim/sampling_rate
+#waveform window
+waveform_window=300 #ms
 
-time_vector = np.arange(0,len(data[0])/sampling_rate,1/sampling_rate)
 
-plt.plot(time_vector,signal[2])
-for i in stim_times:
-    plt.axvline(i)
+noise_window=50
+std_threshold = 5 #Times the std
+distance = 50 # distance between 2 spikes
 
-for i in range(len(stim_times)):
-    if i != 0:
-        print(stim_times[i]-stim_times[i-1])
 
-data_cmr=data_cmr.reshape(int(len(raw_file)/16),-1).transpose()
 
+
+"""
+Functions
 """
-Setups
-"""
-raw_path=rf'{folderpath}/raw/'
-stim_idx_path=rf'{folderpath}/raw/'
-
-files_to_analyse=[]
-
-for file_name in os.listdir(raw_path):
-    if file_name.endswith('.rbf') and Animal in file_name and not "filtered" in file_name and not "concatenated" in file_name:
-        files_to_analyse.append(rf'{raw_path}/{file_name}')
-
-if Save ==True:
-    plot_save = rf"{folderpath}/plots"
-    isExist = os.path.exists(plot_save)
-    if not isExist:
-        os.makedirs(plot_save) #Create folder for the experience if it is not already done
 
 
 
-for file_to_analyze in files_to_analyse:
+
+
+def extract_waveform(signal_file, stim_idx_file, waveform_window):
     
-    raw_file = np.fromfile(file_to_analyze)
+    '''
+    Function to extract spikes waveforms in spike2 recordings
     
-    name_file=file_to_analyze.split('/')[-1].split('.')[0]
+    INPUTS :
+
     
-    stim_idx_file=
+    OUTPUTS : 
+
     
-    print(name_file)
+    '''
     
+    waveforms=[]
     
+    signal=np.fromfile(signal_file)*1000 #in mV
+    signal=signal.reshape(int(len(signal)/number_of_channel),-1).transpose()
     
-    data = raw_file.reshape(int(len(raw_file)/16),-1).transpose()
-        
-    if Preprocessed == True:
-        preprocessed_path=rf"{file_to_analyze.split('raw')[0]}/preprocessed"
-        path_filter = rf"{preprocessed_path}/{name_file}_filtered.rbf"
-        path_cmr = rf"{preprocessed_path}/{name_file}_cmr.rbf"
-        
-        data_filtered=np.fromfile(path_filter)
-        data_filtered = data_filtered.reshape(int(len(raw_file)/16),-1).transpose()
-        
-        data_cmr=np.fromfile(path_cmr)
-        data_cmr=data_cmr.reshape(int(len(raw_file)/16),-1).transpose()
+    stim_indexes=np.loadtxt(stim_idx_file)
+    stim_indexes_corrected = stim_indexes/(sampling_rate_stim/sampling_rate)
+
     
-    time_vector = np.arange(0,len(data[0])/sampling_rate,1/sampling_rate)
+    """
+    Drop le premier index ?
+    
+    stim_indexes[5]-stim_indexes[4] = 1049300 ou 1049350 = 1 sec
+    
+    """
     
        
+    window_idx=int((waveform_window/1000)*20000) 
     
-    """
-    Plot every channel (raw, filtered, cmr) on individual plot
-    """
-    if Indivdual_plots==True:
+    for chan in range(number_of_channel):
+        chan_signal=signal[chan]
+        chan_wf=[]
         
-        for i in range(len(data)):
-            plt.figure()
-            plt.title(rf'Channel {i}')
-            # plt.plot(time_vector,data[i,:],alpha=1,linewidth=1)
-            if Preprocessed == True:
-                plt.plot(time_vector,data_filtered[i,:],alpha=0.5)
-                plt.plot(time_vector,data_cmr[i,:])
-                ax = plt.gca()
-                ax.set_ylim(-0.0025,0.0025)
+        for i in range(len(stim_indexes_corrected)): 
+            index = stim_indexes_corrected[i]
+
+            stim_wf = chan_signal[int(index) : int(index+window_idx)]
+    
+            chan_wf.append(stim_wf)
             
-            if Save == True:
-                plt.savefig(rf'{plot_save}/{name_file}_{i}.{plot_format}')
-                
-        if Autoclose == True:
-            plt.close('all')
-        
-    """
-    Plot all channel cmr on 1 plot
-    """   
-    if Preprocessed == True:
-        fig1, axs = plt.subplots(len(data_cmr),sharex=True,sharey=True)
-        fig1.suptitle(f'{file_to_analyze.split("/")[-1]} CMR of all channels')
-        for i in range(len(data_cmr)):
-            axs[i].plot(time_vector,data_cmr[i,:])
-            axs[i].get_yaxis().set_visible(False)
-        
-        if Autoclose == True:
-            plt.close()    
+        waveforms.append(chan_wf)
     
-    """
-    Plot all channel raw on 1 plot
-    """
-    fig2, axs = plt.subplots(len(data),sharex=True,sharey=True)
-    fig2.suptitle(f'{file_to_analyze.split("/")[-1]} Raw signal of all channels')
-    for i in range(len(data)):
-        axs[i].plot(time_vector,data[i,:])
-        axs[i].get_yaxis().set_visible(False)
-    if Autoclose == True:
-        plt.close()        
-    """
-    Save plots
-    """
-    if Save==True:
-        if Preprocessed == True:
-            fig1.savefig(rf'{plot_save}/{name_file}_cmr.{plot_format}')
-        fig2.savefig(rf'{plot_save}/{name_file}_raw.{plot_format}')
+    time_vector = (np.arange(0,window_idx/sampling_rate,1/sampling_rate))*1000
+    
+    return waveforms,time_vector
+
+
+
+# plt.plot(signal[3])
+
+# for stim in stim_indexes_corrected:
+#     plt.axvline(stim, c='r')
+
+waveforms,time_vector = extract_waveform(signal_file,stim_idx_file,waveform_window)
+
+for chan in waveforms:
+    SPIKE_Y=[]
+    SPIKE_X=[]
+    
+    plt.figure()
+    med=np.median(chan, axis=0)
+    for stim in chan:
+        plt.plot(time_vector,stim,alpha=0.5)
+        
+        # Threshold calculation
+        noise = stim[-int(noise_window*sampling_rate):] #noise window taken from individual channel signal
+        threshold = np.median(noise)+std_threshold*np.std(noise) #threshold calculation for the channel
+        
+        stim_tronc=stim
+        #Detect the spike indexes
+        spike_idx, _ = sp.find_peaks(-stim_tronc,height=threshold,distance=distance)
+        #Convert to spike times
+        spike_times = spike_idx*1./sampling_rate*1000
+        #Get spikes peak 
+        spike_y = stim[spike_idx]
+        
+        SPIKE_Y.append(spike_y)
+        SPIKE_X.append(spike_times)
+        
+
+        
+        
+    
+for chan in waveforms:
+    # Threshold calculation
+    noise = chan[-int(noise_window*sampling_rate):] #noise window taken from individual channel signal
+    threshold = np.median(noise)+std_threshold*np.std(noise) #threshold calculation for the channel
+
+    #Detect the spike indexes
+    spike_idx, _ = sp.find_peaks(-chan,height=threshold,distance=distance)
+    #Convert to spike times
+    spike_times = spike_idx*1./sampling_rate
+    #Get spikes peak 
+    spike_y = chan[spike_idx]
+    
+    #Append spikes times to the list of all channels spikes
+    spikes_list.append(spike_times)
+    spikes_list_y.append(spike_y)
+    
+    if Waveforms == True :
+        wfs = extract_spike_waveform(signal,spike_idx)
+        waveforms.append(wfs)
