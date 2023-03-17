8aaecba9e4edb4e733e97c43605d52f2068b6ab9
Gilles <gillesdelbecq@hotmail.fr>
2023-03-17 18:52:04 +0100

    update raster 17-03

--- In vivo Multi/intan_test_optotag.py
+++ In vivo Multi/intan_test_optotag.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#! /bin/env python
 """
 Created on Tue Feb 28 16:06:17 2023
 
@@ -10,6 +11,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import scipy.signal as sp
+
+from intanutil.read_header import read_header
+from intanutil.get_bytes_per_data_block import get_bytes_per_data_block
+from intanutil.read_one_data_block import read_one_data_block
+from intanutil.notch_filter import notch_filter
+from intanutil.data_to_result import data_to_result
+
+import pandas as pd
 
 
 def read_data(filename):
@@ -205,64 +214,7 @@ def plural(n):
         return 's'
 
 
-
-path="//equipe2-nas1/Gilles.DELBECQ/Data/ePhy/Février2023/Test_Gustave/raw/raw intan/Test_Gustave_15_03_230315_182841/Test_Gustave_15_03_230315_182841.rhd"
-
-reader=read_data(path)
-
-sampling_rate = reader['frequency_parameters']['amplifier_sample_rate']
-time_vector=reader['t_amplifier']
-signal=reader['amplifier_data']
-
-selected_channels=['2','3','4','9','10','11','12','14','15']
-
-#Filtering parameters
-freq_low = 300
-freq_high = 3000
-order = 4
-
-
-# Noise parameters
-std_threshold = 5 #Times the std
-noise_window = 5 #window for the noise calculation in sec
-distance = 50 # distance between 2 spikes
-
-#waveform window
-waveform_window=5 #ms
-Waveforms = True
-
-def extract_spike_waveform(signal, spike_idx, left_width=(waveform_window/1000)*20000/2, right_width=(waveform_window/1000)*20000/2):
-    
-    '''
-    Function to extract spikes waveforms in spike2 recordings
-    
-    INPUTS :
-        signal (1-d array) : the ephy signal
-        spike_idx (1-d array or integer list) : array containing the spike indexes (in points)
-        width (int) = width for spike window
-    
-    OUTPUTS : 
-        SPIKES (list) : a list containg the waveform of each spike 
-    
-    '''
-    
-    SPIKES = []
-    
-    left_width = int(left_width)
-    right_width = int(right_width)
-    
-    for i in range(len(spike_idx)): 
-        index = spike_idx[i]
-
-        spike_wf = signal[index-left_width : index+right_width]
-
-        SPIKES.append(spike_wf)
-    return SPIKES
-
-
-
-
-def filter_signal(signal, order=order, sample_rate=sampling_rate, freq_low=freq_low, freq_high=freq_high, axis=0):
+def filter_signal(signal, order=4, sample_rate=20000, freq_low=300, freq_high=3000, axis=0):
     """
     From Théo G.
     Filtering with scipy
@@ -278,18 +230,39 @@ def filter_signal(signal, order=order, sample_rate=sampling_rate, freq_low=freq_
     return filtered_signal
 
 
-# def notch_filter(signal, order=4, sample_rate=20000, freq_low=48, freq_high=52, axis=0):
-#     import scipy.signal
-#     Wn = [freq_low / (sample_rate / 2), freq_high / (sample_rate / 2)]
-#     notch_coeff = scipy.signal.iirfilter(order, Wn, btype="bandstop", ftype="butter", output="sos")
-#     notch_signal = scipy.signal.sosfiltfilt(notch_coeff, signal, axis=axis)
 
-#     return notch_signal
+"""
+Load intan file
+"""
+path=r"//equipe2-nas1/Gilles.DELBECQ/Data/ePhy/Février2023/Test_Gustave/raw/raw intan/Test_Gustave_16_03_230316_165717/merged.rhd"
+savepath=r'\\equipe2-nas1\Gilles.DELBECQ\Data\ePhy\Février2023\Test_Gustave\figures_optotag'
+save_prefix='16_03_optotag_5ms_2Hz'
 
+
+reader=read_data(path)
+sampling_rate = reader['frequency_parameters']['amplifier_sample_rate']
+time_vector=reader['t_amplifier']
+signal=reader['amplifier_data']
+dig_inputs=reader['board_dig_in_data'][1]
+
+
+
+"""
+Récupérer l'index des stims'
+"""
+stim_idx=[]
+for i in range(len(dig_inputs)):
+    if dig_inputs[i] and (i==0 or not dig_inputs[i-1]):
+        stim_idx.append(i)
+
+
+
+"""
+Filtering
+"""
 filtered_signals=[]
 
-for i in selected_channels:
-    
+for i in range(len(signal)):
     filtered_signal=filter_signal(signal[int(i),:])
     filtered_signals.append(filtered_signal)
     # plt.figure()
@@ -301,20 +274,20 @@ filtered_signals = np.array(filtered_signals)
 median = np.median(filtered_signals, axis=0)#compute median on all 
 cmr_signals = filtered_signals-median     #compute common ref removal median on all 
 
-for i in range(len(cmr_signals)):
-    plt.figure()
-    plt.plot(time_vector,cmr_signals[i])
-    plt.title(rf'channel {int(selected_channels[i])}')
-    
+
 
 """
 Spike detection
 """
-thresholds=[]
-spikes_list=[]
-spikes_list_y=[]
-wfs=[]
-waveforms=[]
+# Noise parameters
+std_threshold = 4 #Times the std
+noise_window = 2 #window for the noise calculation in sec
+distance = 50 # distance between 2 spikes
+
+waveform_window=10#⌡in ms
+
+thresholds,spikes_list,spikes_list_y,spikes_times_list,wfs,waveforms=[],[],[],[],[],[]
+
 for signal in cmr_signals:
     # Threshold calculation
     noise = signal[0:int(noise_window*sampling_rate)] #noise window taken from individual channel signal
@@ -330,12 +303,13 @@ for signal in cmr_signals:
     spike_y = signal[spike_idx]
     
     #Append spikes times to the list of all channels spikes
-    spikes_list.append(spike_times)
+    spikes_list.append(spike_idx)
+    spikes_times_list.append(spike_times)
     spikes_list_y.append(spike_y)
     
-    if Waveforms == True :
-        wfs = extract_spike_waveform(signal,spike_idx)
-        waveforms.append(wfs)
+    # if Waveforms == True :
+    #     wfs = extract_spike_waveform(signal,spike_idx)
+    #     waveforms.append(wfs)
 
 for index,i in np.ndenumerate(waveforms):
     plt.figure()
@@ -344,3 +318,129 @@ for index,i in np.ndenumerate(waveforms):
     for j in i:
         plt.plot(j*1000)
     # plt.savefig(rf'{save_path}\waveform_chan_{selected_chan[index[0]]}.svg')
+
+
+
+
+"""
+Raster plot
+
+"""
+for Channel_to_analyze in range(16):
+    
+    
+    event_indices=spikes_list[Channel_to_analyze]
+    signal=cmr_signals[Channel_to_analyze]
+    
+    
+    # Durée de la fenêtre avant et après chaque stimulation (en nombre de points)
+    pre_stim_window_size = int(10*sampling_rate/1000)
+    post_stim_window_size = int(50*sampling_rate/1000)
+    
+    # Durée de chaque fenêtre (en nombre de points)
+    window_size = int(pre_stim_window_size + post_stim_window_size)
+    
+    # Création du tableau 2D pour stocker les fenêtres de signal
+    num_stimulations = len(stim_idx)
+    num_events = len(event_indices)
+    signal_windows = np.zeros((num_events, num_stimulations, window_size))
+    
+    num_windows = num_stimulations
+    
+    event_times = spikes_times_list
+    stim_times = np.array(stim_idx)/sampling_rate*1000
+    
+    plt.figure()
+    plt.plot(time_vector[500:100000],signal[500:100000])
+
+    # for spike in event_indices/sampling_rate:
+    #     plt.axvline(spike,color='red')
+    
+    
+    """
+    #Raster plot global
+    plt.figure()
+    plt.eventplot(event_times, colors='k')
+    """
+    
+    
+    # Indices des pics
+    peak_indices = spikes_list[Channel_to_analyze]
+    
+    # Nombre total de fenêtres
+    num_windows = len(stim_idx)
+    
+    # Créer une matrice de zéros pour stocker les fenêtres de signal
+    signal_windows = np.zeros((num_windows, pre_stim_window_size + post_stim_window_size))
+    
+    # Remplir la matrice avec les fenêtres de signal
+    for i, stim_index in enumerate(stim_idx):
+        signal_windows[i, :] = signal[stim_index - pre_stim_window_size : stim_index + post_stim_window_size]
+    
+    # Créer une matrice de zéros pour stocker les événements
+    event_matrix = np.zeros_like(signal_windows)
+    
+    # Trouver les indices des pics dans chaque fenêtre de signal
+    for i in range(num_windows):
+        peak_indices_in_window = peak_indices[(peak_indices >= stim_idx[i] - pre_stim_window_size) & (peak_indices < stim_idx[i] + post_stim_window_size)]
+        if len(peak_indices_in_window) >0:
+            event_matrix[i, peak_indices_in_window - (stim_idx[i] - pre_stim_window_size)] = 1
+    
+    
+    
+    
+    from itertools import groupby
+    
+    # votre array
+    arr = np.argwhere(event_matrix == 1)
+    
+    # Regrouper les valeurs de la deuxième colonne par les valeurs de la première colonne
+    grouped_array = []
+    for i in np.unique(arr[:, 0]):
+        grouped_array.append(list(arr[arr[:, 0] == i, 1]))
+    
+    # Créer un nouveau 2D array avec les valeurs de la deuxième colonne regroupées
+    new_array = np.array([np.array(x) for x in grouped_array])
+    
+    
+    
+    
+    
+    # Afficher le raster plot
+    plt.figure()
+    plt.eventplot(np.array(new_array), colors='k')
+    
+    # Ajouter une ligne verticale pour chaque stimulation
+    # plt.axvline(200, color='r', linestyle='--')
+    plt.axvspan(200,300,color='blue',alpha=0.2)
+    
+    # Ajouter une étiquette pour l'axe des y
+    plt.ylabel('Stimulation')
+    plt.title(rf'Channel : {Channel_to_analyze}')
+
+    plt.savefig(rf"{savepath}/{save_prefix}_Channel_{Channel_to_analyze}.png")
+    
+    
+    test= pd.DataFrame(grouped_array)
+    test.to_excel(rf"{savepath}/{save_prefix}_Channel_{Channel_to_analyze}.xlsx")
+    
+    
+    num_events = int(np.sum(event_matrix))
+    event_times = np.where(event_matrix == 1)
+    
+    # events_to_plot=event_times[1]
+    
+    events_to_plot=np.where((event_times[1] >= 195) & (event_times[1] <= 205), np.nan, event_times[1])
+    events_to_plot=np.where((events_to_plot >= 295) & (events_to_plot <= 305), np.nan, events_to_plot)
+    
+    
+    bins_in_ms=0.5
+    plt.figure()
+    plt.hist(events_to_plot, bins=int(1200/bins_in_ms/sampling_rate*1000))
+    plt.axvspan(200,300,color='blue',alpha=0.2)
+    plt.xlim(0,500)
+    plt.title(rf'Channel {Channel_to_analyze} bins = {bins_in_ms}ms')
+    plt.savefig(rf"{savepath}/{save_prefix}_Hist_Channel_{Channel_to_analyze}_no_artefact.svg")
+    plt.savefig(rf"{savepath}/{save_prefix}_Hist_Channel_{Channel_to_analyze}_no_artefact.png")
+    
+    
