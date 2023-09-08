68e8c203c3e873d2e3cc2d369098f28cbb215fbf
Gilles <gillesdelbecq@hotmail.fr>
2023-09-08 16:51:51 +0200

    Update Step6 - Plots.py

--- In vivo Multi/June_23_Analysis/Pipeline 07-08/Step6 - Plots.py
+++ In vivo Multi/June_23_Analysis/Pipeline 07-08/Step6 - Plots.py
@@ -18,13 +18,16 @@ import numpy as np
 
 #%% Read dataframes with infos
 
-df_unit_info =pd.read_excel("D:/ePhy/SI_Data/spikesorting_results/0026_02_08/kilosort3/curated/waveforms/units_infos.xlsx")
+session = "0022_01_08"
 
-df_optotag_info = pd.read_excel('D:/ePhy/SI_Data/spikesorting_results/0026_02_08/kilosort3/curated/optotag/optotag_infos.xlsx')
 
-df_correlation_speed = pd.read_excel('D:/ePhy/SI_Data/spikesorting_results/0026_02_08/kilosort3/curated/correlation.xlsx',sheet_name='Speed')
-df_correlation_obst =pd.read_excel('D:/ePhy/SI_Data/spikesorting_results/0026_02_08/kilosort3/curated/correlation.xlsx',sheet_name='Obstacle')
-df_correlation_z = pd.read_excel('D:/ePhy/SI_Data/spikesorting_results/0026_02_08/kilosort3/curated/correlation.xlsx', sheet_name='Back_Z')
+df_unit_info =pd.read_excel(rf"D:/ePhy/SI_Data/spikesorting_results/{session}/kilosort3/curated/processing_data/units_metrics.xlsx")
+
+df_optotag_info = pd.read_excel(rf'D:/ePhy/SI_Data/spikesorting_results/{session}/kilosort3/curated/processing_data/optotag_infos.xlsx')
+
+df_correlation_speed = pd.read_excel(rf'D:/ePhy/SI_Data/spikesorting_results/{session}/kilosort3/curated/correlation.xlsx',sheet_name='Speed')
+df_correlation_obst =pd.read_excel(rf'D:/ePhy/SI_Data/spikesorting_results/{session}/kilosort3/curated/correlation.xlsx',sheet_name='Obstacle')
+df_correlation_z = pd.read_excel(rf'D:/ePhy/SI_Data/spikesorting_results/{session}/kilosort3/curated/correlation.xlsx', sheet_name='Back_Z')
 
 unit_list = df_unit_info['Unit']
 
@@ -74,4 +77,40 @@ plt.show()
 
     
     
-#%% Plot 3 : 
+#%% Plot 3 : PCA spike_params
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+data = df_unit_info.iloc[:, 4:]
+
+scaler = StandardScaler()
+data_scaled = scaler.fit_transform(data)
+
+pca = PCA()
+principal_components = pca.fit_transform(data_scaled)
+
+explained_variance_ratio = pca.explained_variance_ratio_
+cumulative_variance_ratio = np.cumsum(explained_variance_ratio)
+
+plt.plot(range(1, len(cumulative_variance_ratio) + 1), cumulative_variance_ratio, marker='o', linestyle='--')
+plt.xlabel('Nombre de composantes principales')
+plt.ylabel('Variance expliquée cumulée')
+plt.show()
+
+# Par exemple, si vous voulez capturer 95% de la variance
+n_components = np.argmax(cumulative_variance_ratio >= 0.95) + 1
+
+# Appliquez à nouveau la PCA avec le nombre de composantes sélectionné
+pca = PCA(n_components=n_components)
+principal_components = pca.fit_transform(data_scaled)
+
+from scipy.cluster import hierarchy
+
+dendrogram = hierarchy.dendrogram(hierarchy.linkage(principal_components, method='ward'))
+plt.title('Dendrogramme')
+plt.xlabel('Unités')
+plt.ylabel('Distance')
+plt.show()
