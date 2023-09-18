0ffda50bf8a437bbce4969e1b2a1581c573a3da5
Gilles <gillesdelbecq@hotmail.fr>
2023-09-18 18:34:53 +0200

    Update plot_raster_heatmap_speed.py

--- In vivo Multi/June_23_Analysis/Pipeline 07-08/wip/plot_raster_heatmap_speed.py
+++ In vivo Multi/June_23_Analysis/Pipeline 07-08/wip/plot_raster_heatmap_speed.py
@@ -53,13 +53,39 @@ def Check_Save_Dir(save_path):
     return
 
 #%% 2. Load the datasets
-spike_times = pd.read_excel(r"//equipe2-nas1/Public/DATA/Gilles/Spikesorting_August_2023/SI_Data/spikesorting_results/0022_01_08/kilosort3/curated/processing_data/0022_01_08_spike_times.xlsx")
+spike_times = pd.read_excel(r"//equipe2-nas1/Public/DATA/Gilles/Spikesorting_August_2023/SI_Data/spikesorting_results/0022_01_08/kilosort3/curated/processing_data/spike_times.xlsx")
 
 mocap_files = list_recording_files(r'//equipe2-nas1/Public/DATA/Gilles/Spikesorting_August_2023/SI_Data/spikesorting_results/0022_01_08/kilosort3/curated/processing_data/sync_data_rate_sigma_20.0 msms_Gaussian')
 
 
 save_folder = r"\\equipe2-nas1\Public\DATA\Gilles\Spikesorting_August_2023\SI_Data\spikesorting_results\0022_01_08\kilosort3\curated\processing_data\plots/Speed_Rasterplots" 
 
+
+#%% Rasterplot whole session
+
+# 4.2. Create a raster plot for spike times on the bottom subplot
+for idx, column in enumerate(spike_times.columns[1:]):
+    times = spike_times[column].dropna().values
+    # Only include spike times within the range of the mocap data
+    plt.scatter(times, [idx] * len(times), marker='|', color='black')
+
+plt.yticks(range(len(spike_times.columns[1:])),spike_times.columns[1:])
+
+for file in mocap_files:
+    trial = file.split('_')[-2]
+    session = file.split('_')[-3]
+    mocap_data = pd.read_excel(file)
+    
+    # 3. Filter the mocap_data to include all rows between the first and last non-NaN speed_back1
+    filtered_mocap_data = mocap_data.dropna(subset=['speed_back1'])
+    start_index = filtered_mocap_data.index[0]
+    end_index = filtered_mocap_data.index[-1]
+    
+    plt.axvspan(filtered_mocap_data['time_axis'][start_index], filtered_mocap_data['time_axis'][end_index], alpha=0.3)
+    
+    
+
+#%% Raster plots by trial
 
 for file in mocap_files:
     trial = file.split('_')[-2]
@@ -104,8 +130,8 @@ for file in mocap_files:
     
     
     
-    
-    
+    """
+    #Heatmap test
     # 1. Define parameters for binning
     bin_size = 0.05  # 50ms
     time_bins = np.arange(subsampled_mocap_data['time_axis'].iloc[0], 
@@ -150,3 +176,5 @@ for file in mocap_files:
     save_path = rf"{save_folder}/heatmap_{session}_{trial}"
     Check_Save_Dir(save_path)
     plt.savefig(save_path)
+    
+    """
