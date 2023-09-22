878640a891a8a3e4fea8f8b701690eac9730ee21
Gilles <gillesdelbecq@hotmail.fr>
2023-09-22 18:33:13 +0200

    Update concatenate_rates_mocap_by_trial.py

--- In vivo Multi/June_23_Analysis/Pipeline 07-08/wip/concatenate_rates_mocap_by_trial.py
+++ In vivo Multi/June_23_Analysis/Pipeline 07-08/wip/concatenate_rates_mocap_by_trial.py
@@ -42,28 +42,38 @@ mocap_session_name = "0022_01"
 
 
 list_of_trials = [14, 15, 16, 17, 18, 19, 20, 21, 22]
+# list_of_trials = [3,4,5,6,7,8,9,10,11,12,13]
 
 # List of columns to focus on
 focus_columns = [
-    "left_foot_x", "left_foot_y", "left_foot_z",
-    "left_ankle_x", "left_ankle_y", "left_ankle_z",
+    "left_foot_x", "left_foot_y", "left_foot_z", 
+    "left_ankle_x", "left_ankle_y", "left_ankle_z", 
     "left_knee_x", "left_knee_y", "left_knee_z",
-    "left_hip_x", "left_hip_y", "left_hip_z",
-    "right_foot_x", "right_foot_y", "right_foot_z",
+    "left_hip_x", "left_hip_y", "left_hip_z", 
+    "right_foot_x", "right_foot_y", "right_foot_z", 
     "right_ankle_x", "right_ankle_y", "right_ankle_z",
-    "right_knee_x", "right_knee_y", "right_knee_z",
-    "right_hip_x", "right_hip_y", "right_hip_z",
-    "left_ankle_angle", 
-    "left_knee_angle",
-    "left_hip_angle",
-    "right_ankle_angle", 
-    "right_knee_angle", 
-    "right_hip_angle",
-    "back1_x", "back1_y", "back1_z",
-    "back2_x", "back2_y", "back2_z",
-    "back_orientation", "back_inclination", "back_1_Z", "back_2_Z",
-    "speed_back1", "speed_left_foot", "speed_right_foot",
-    # "obstacle_x", "obstacle_y", "obstacle_z","distance_from_obstacle",
+    "right_knee_x", "right_knee_y", "right_knee_z", 
+    "right_hip_x", "right_hip_y", "right_hip_z", 
+    
+    "left_foot_x_norm", "left_foot_y_norm", "left_foot_z_norm",
+    "left_ankle_x_norm", "left_ankle_y_norm", "left_ankle_z_norm",
+    "left_knee_x_norm", "left_knee_y_norm", "left_knee_z_norm", 
+    "left_hip_x_norm", "left_hip_y_norm", "left_hip_z_norm",
+    "right_foot_x_norm", "right_foot_y_norm", "right_foot_z_norm",
+    "right_ankle_x_norm","right_ankle_y_norm", "right_ankle_z_norm",
+    "right_knee_x_norm", "right_knee_y_norm", "right_knee_z_norm",
+    "right_hip_x_norm", "right_hip_y_norm", "right_hip_z_norm",
+    
+    "left_ankle_angle", "left_knee_angle", "left_hip_angle",
+    "right_ankle_angle", "right_knee_angle", "right_hip_angle",
+    
+    "back1_x", "back1_y", "back1_z", "back2_x", "back2_y", "back2_z",
+    "back_orientation", "back_inclination",
+    
+    "speed_back1", "speed_left_foot", "speed_right_foot", 
+    "obstacle_x", "obstacle_y", "obstacle_z", 
+
+    "distance_from_obstacle", "distance_from_obstacle_x", 
     # "distance_from_start", "distance_from_end"
 ]
 
@@ -85,7 +95,7 @@ for trial in list_of_trials:
     
     
     # Filter mocap_df to only include rows from the first non-missing "back_1_Z" value to the last non-missing "back_1_Z" value
-    filtered_mocap_df = merged_df[merged_df['back_1_Z'].first_valid_index(): merged_df['back_1_Z'].last_valid_index() + 1]
+    filtered_mocap_df = merged_df[merged_df['back1_z'].first_valid_index(): merged_df['back1_z'].last_valid_index() + 1]
     
     
     #TODO : wtf?
