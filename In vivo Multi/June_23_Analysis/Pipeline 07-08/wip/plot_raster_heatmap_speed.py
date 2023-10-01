d2459c41608cb14d3562d2293a1606db5c83a8d5
Gilles <gillesdelbecq@hotmail.fr>
2023-10-01 15:14:20 +0200

    Update plot_raster_heatmap_speed.py

--- In vivo Multi/June_23_Analysis/Pipeline 07-08/wip/plot_raster_heatmap_speed.py
+++ In vivo Multi/June_23_Analysis/Pipeline 07-08/wip/plot_raster_heatmap_speed.py
@@ -53,12 +53,12 @@ def Check_Save_Dir(save_path):
     return
 
 #%% 2. Load the datasets
-spike_times = pd.read_excel(r"//equipe2-nas1/Public/DATA/Gilles/Spikesorting_August_2023/SI_Data/spikesorting_results/0022_01_08/kilosort3/curated/processing_data/spike_times.xlsx")
+spike_times = pd.read_excel(r"E:/Data/ephy/Spikesorting August 2023/SI_Data/spikesorting_results/0022_01_08/kilosort3/curated/processing_data/spike_times.xlsx")
 
-mocap_files = list_recording_files(r'//equipe2-nas1/Public/DATA/Gilles/Spikesorting_August_2023/SI_Data/spikesorting_results/0022_01_08/kilosort3/curated/processing_data/sync_data_rate_sigma_20.0 msms_Gaussian')
+mocap_files = list_recording_files(r'E:/Data/ephy/Spikesorting August 2023/SI_Data/spikesorting_results/0022_01_08/kilosort3/curated/processing_data/sync_data_rate_sigma_20.0 msms_Gaussian')
 
-behavior_to_plot = 'right_ankle_angle'
-save_folder = rf"\\equipe2-nas1\Public\DATA\Gilles\Spikesorting_August_2023\SI_Data\spikesorting_results\0022_01_08\kilosort3\curated\processing_data\plots/{behavior_to_plot}_Rasterplots" 
+behavior_to_plot = 'speed_back1'
+save_folder = rf'E:/Data/ephy/Spikesorting August 2023/SI_Data/spikesorting_results/0022_01_08/kilosort3/curated/processing_data\plots/{behavior_to_plot}_Rasterplots'
 
 
 #%% Rasterplot whole session
@@ -130,7 +130,102 @@ for file in mocap_files:
     Check_Save_Dir(os.path.dirname(save_path))
     plt.savefig(save_path)
     
+
+for file in mocap_files:
+    trial = file.split('_')[-2]
+    session = file.split('_')[-3]
+    mocap_data = pd.read_excel(file)
     
+    # 3. Filter the mocap_data to include all rows between the first and last non-NaN speed_back1
+    filtered_mocap_data = mocap_data.dropna(subset=['speed_back1'])
+    start_index = filtered_mocap_data.index[0]
+    end_index = filtered_mocap_data.index[-1]
+    subsampled_mocap_data = mocap_data.iloc[start_index:end_index+1]
+    
+    # 4. Begin plotting
+    fig, axs = plt.subplots(3, 1, figsize=(12, 12), sharex=True)
+    
+    # 4.1. Plot the speed_back1 data on the top subplot
+    axs[0].plot(subsampled_mocap_data['time_axis'], subsampled_mocap_data[behavior_to_plot], color='blue')
+    axs[0].set_title(rf'{behavior_to_plot} {session}_{trial}')
+    axs[0].set_ylabel('mm')
+    
+    # 4.2. Create a raster plot for spike times on the second subplot
+    for idx, column in enumerate(spike_times.columns[1:]):
+        times = spike_times[column].dropna().values
+        # Only include spike times within the range of the mocap data
+        times = times[(times >= subsampled_mocap_data['time_axis'].iloc[0]) & (times <= subsampled_mocap_data['time_axis'].iloc[-1])]
+        axs[1].scatter(times, [idx] * len(times), marker='|', color='black')
+    
+    # 4.3. Adjust raster plot aesthetics
+    axs[1].set_yticks(range(len(spike_times.columns[1:])))
+    axs[1].set_yticklabels(spike_times.columns[1:])
+    axs[1].set_title('Raster plot of spike times')
+    axs[1].set_ylabel('Units')
+    
+    # 4.4. Create a histogram of events on the third subplot
+    all_spike_times = [spike_times[column].dropna().values for column in spike_times.columns[1:]]
+    all_spike_times = np.concatenate(all_spike_times)
+    axs[2].hist(all_spike_times, bins=50, color='gray')
+    axs[2].set_title('Histogram of spike events')
+    axs[2].set_ylabel('Number of Spikes')
+    axs[2].set_xlabel('Time (s)')
+
+    # 5. Adjust layout and display the plot
+    plt.tight_layout()
+    plt.show()
+    
+    # save_path = rf"{save_folder}/raster_{session}_{trial}"
+    # Check_Save_Dir(os.path.dirname(save_path))
+    # plt.savefig(save_path)
+
+for file in mocap_files:
+    trial = file.split('_')[-2]
+    session = file.split('_')[-3]
+    mocap_data = pd.read_excel(file)
+    
+    # 3. Filter the mocap_data to include all rows between the first and last non-NaN speed_back1
+    filtered_mocap_data = mocap_data.dropna(subset=['speed_back1'])
+    start_index = filtered_mocap_data.index[0]
+    end_index = filtered_mocap_data.index[-1]
+    subsampled_mocap_data = mocap_data.iloc[start_index:end_index+1]
+    
+    # 4. Begin plotting
+    fig, axs = plt.subplots(3, 1, figsize=(12, 12), sharex=True)
+    
+    # 4.1. Plot the speed_back1 data on the top subplot
+    axs[0].plot(subsampled_mocap_data['time_axis'], subsampled_mocap_data[behavior_to_plot], color='blue')
+    axs[0].set_title(rf'{behavior_to_plot} {session}_{trial}')
+    axs[0].set_ylabel('mm')
+    
+    # 4.2. Create a raster plot for spike times on the second subplot
+    for idx, column in enumerate(spike_times.columns[1:]):
+        times = spike_times[column].dropna().values
+        # Only include spike times within the range of the mocap data
+        times = times[(times >= subsampled_mocap_data['time_axis'].iloc[0]) & (times <= subsampled_mocap_data['time_axis'].iloc[-1])]
+        axs[1].scatter(times, [idx] * len(times), marker='|', color='black')
+    
+    # 4.3. Adjust raster plot aesthetics
+    axs[1].set_yticks(range(len(spike_times.columns[1:])))
+    axs[1].set_yticklabels(spike_times.columns[1:])
+    axs[1].set_title('Raster plot of spike times')
+    axs[1].set_ylabel('Units')
+    
+    # 4.4. Create a histogram of events for the first unit on the third subplot
+    first_unit_spikes = spike_times[spike_times.columns[1]].dropna().values
+    # Filter spike times to be within the mocap data range
+    first_unit_spikes = first_unit_spikes[(first_unit_spikes >= subsampled_mocap_data['time_axis'].iloc[0]) & 
+                                          (first_unit_spikes <= subsampled_mocap_data['time_axis'].iloc[-1])]
+    axs[2].hist(first_unit_spikes, bins=50, color='gray')
+    axs[2].set_title('Histogram of spike events for first unit')
+    axs[2].set_ylabel('Number of Spikes')
+    axs[2].set_xlabel('Time (s)')
+
+    # 5. Adjust layout and display the plot
+    plt.tight_layout()
+    plt.show()
+ 
+
     
     """
     #Heatmap test
